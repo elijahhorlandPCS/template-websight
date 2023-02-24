9173e35c2ba3df443ea022eb0fd2348f4bcb584b
Elijah Horland <elijah@Brad-Talbots-iphone.local>
2023-02-24 10:29:42 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+//const mutton = document.querySelector("#mutton");
+
+//mutton.addEventListener("click", function1);
+
+
+
+const mutton = document.querySelector("#mutton")
+
+    
+
+mutton.addEventListener("click", function1);
+
+let color = 0
+
+
+let array1 = document.querySelectorAll(".repimg")
+
+function function1(){
+
+    if (color === 0){
+        document.body.style.backgroundImage = 'url("stupidlookinguy.jpg")';
+        
+        color = 1
+        console.log(color)
+mutton.innerHTML = "PUT HIM BACK"
+
+
+
+//document.getElementById("repimg").src="..."
+
+
+
+for (let i = 0; i < array1.length; array1[i].src="...", i++)
+
+
+
+//console.log(document.querySelectorAll(".repimg"))
+
+console.log(array1)
+
+
+
+    } else if(color === 1){
+        document.body.style.backgroundImage = 'url("")';
+        color = 0
+        console.log(color)
+
+        for (let i = 0; i < array1.length; array1[i].src="stupidlookinguy.jpg", i++)
+        
+        mutton.innerHTML = "let little lemur guy out"
+    }    
+    
+    
+        }
+
+
